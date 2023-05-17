83f7921bb2836b59c26d07aa260d9bb466ebcd90
Stanislav <tkacenkostas20@gmail.com>
2023-05-17 21:17:52 +0300

    add login and register pages

--- ProjectHive/src/app/app-routing.module.ts
+++ ProjectHive/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MainComponent} from "./components/main/main.component";
+import {MainComponent} from "./main-v1/main/main.component";
+import {LoginComponent} from "./main-v1/modules/auth/login/login.component";
+import {RegistrationComponent} from "./main-v1/modules/auth/registration/registration.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'},
-  {path: 'main', component: MainComponent}
+  {path: 'main', component: MainComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegistrationComponent}
 ];
 
 @NgModule({
--- ProjectHive/src/app/app.module.ts
+++ ProjectHive/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ButtonModule} from "primeng/button";
-import { MainComponent } from './components/main/main.component';
+import { MainComponent } from './main-v1/main/main.component';
 import {SidebarModule} from "primeng/sidebar";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {TooltipModule} from "primeng/tooltip";
+import { LoginComponent } from './main-v1/modules/auth/login/login.component';
+import { RegistrationComponent } from './main-v1/modules/auth/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
